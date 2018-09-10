5d99f3e9b59ccb94abb6f1fe8b63fff99a8b8971
mayvut <mayvut@HN-SD-0311-WK.HarveyNash.vn.local>
2018-09-10 15:03:19 +0700

    Create Bank Guru

---
+++ actions/page/NewCustomerPage.java
@@ -0,0 +1,13 @@
+package page;
+
+import org.openqa.selenium.WebDriver;
+
+import common.AbstractPage;
+
+public class NewCustomerPage extends AbstractPage{
+	public NewCustomerPage(WebDriver driver) {
+		super.driver = driver;
+	}
+	
+
+}
---
+++ testcases/com/guru/login/BankGuru.java
@@ -0,0 +1,40 @@
+package com.guru.login;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import common.AbstractTest;
+import common.Constants;
+import page.BankGuruPage;
+import page.DashboardPage;
+import page.LoginPage;
+
+public class BankGuru extends AbstractTest {
+	String userName = "mngr152551";
+	String password = "derybev";
+	
+	@BeforeClass
+	public void beforeClass() {
+		setUp("ff", Constants.GURU_URL);
+	}
+
+	@Test
+	public void TC01_NameCannotEmpty() throws InterruptedException {
+		// Login page
+		LoginPage loginPage = new LoginPage(driver);
+		DashboardPage dashboardPage = loginPage.login(userName, password);
+		//Thread.sleep(10000);
+		
+		// New Customer 
+		BankGuruPage bankGuruPage = new BankGuruPage(driver);
+		bankGuruPage.goToNewCustomerPage();
+		
+	}
+
+	@AfterClass
+	public void afterClass() {
+		tearDown();
+	}
+}
