bb7cc9b110c620965754acd275857c8da2ccf92c
mayvut <mayvut@HN-SD-0311-WK.HarveyNash.vn.local>
2018-09-10 15:02:45 +0700

    Create New Customer page

--- actions/page/InstanceManager.java
+++ actions/page/InstanceManager.java
@@ -8,7 +8,8 @@ public class InstanceManager {
 
 	private static DashboardPage dashboardPage;
 	private static LoginPage loginPage;
-
+	private static NewCustomerPage newCustomer;
+	
 	public static AbstractPage getInstance(WebDriver driver, String page) {
 		switch (page) {
 		case "LoginPage":
@@ -22,7 +23,11 @@ public class InstanceManager {
 				dashboardPage = new DashboardPage(driver);
 			}
 			return dashboardPage;
-
+		case "NewCustomer":
+			if (newCustomer == null) {
+				newCustomer = new NewCustomerPage(driver);
+			}
+			return dashboardPage;
 		default:
 			return null;
 		}
